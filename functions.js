8cfeb9370c193941482f3edeea00ef02c24443d0
yjeung <yjeung1304@gmail.com>
2018-08-20 23:26:49 +0900

    forms update

--- functions.js
+++ functions.js
@@ -1,36 +1,38 @@
-function toHome(){
+function getForms(page){
+  getHeader();
+  getFooter();
+  if (page!=="home"){
+    getExamplesFixed()
+  }
+}
+function reset(){
   window.location="index.html";
   localStorage.removeItem("sort");
 }
 function getHeader(){
-    var target = document.getElementById("headerMirror");
+    var target = document.createElement("headerMirror");
+    target.id="headerMirror";
     var x = document.getElementById("myFrames");
     var y = (x.contentWindow || x.contentDocument);
     if (y.document)y = y.document;
-    console.log(y);
     target.innerHTML = y.body.querySelector("#header").innerHTML;
-    document.getElementById("toHome").onclick=function(){
-      console.log("reload");
-      toHome();
-    }
-    document.getElementById("toHome").onclick=function(){
-      console.log("reload");
-      toHome();
-    }
+    document.body.appendChild(target);
 }
 function getFooter(){
-    var target = document.getElementById("footerMirror");
+    var target = document.createElement("footerMirror");
+    target.id="footerMirror";
     var x = document.getElementById("myFrames");
     var y = (x.contentWindow || x.contentDocument);
     if (y.document)y = y.document;
-    console.log(y);
     target.innerHTML = y.body.querySelector("#footer").innerHTML;
+    document.body.querySelector("#scrollArea").appendChild(target);
 }
 function getExamplesFixed(){
-    var target = document.getElementById("examplesFixedMirror");
+    var target = document.createElement("examplesFixedMirror");
+    target.id="examplesFixedMirror";
     var x = document.getElementById("myFrames");
     var y = (x.contentWindow || x.contentDocument);
     if (y.document)y = y.document;
-    console.log(y);
     target.innerHTML = y.body.querySelector("#examplesFixed").innerHTML;
+    document.body.appendChild(target);
 }
